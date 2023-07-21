94e6e76b90b205d3cfccd8dfece99a542969a745
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-07-20 23:09:50 -0300

    refactor: valida se e prod ou dev

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,9 +1,5 @@
 import * as dotenv from "dotenv";
 import { DataSource } from "typeorm";
-import { UserEntity } from "../entities/user.entity";
-import { CreateTableUsers1689892033847 } from "../migrations/1689892033847-CreateTableUsers";
-import { TransactionEntity1689895189070 } from "../migrations/1689895189070-TransactionEntity";
-import { TransactionEntity } from "../entities/transaction.entity";
 
 dotenv.config();
 
@@ -24,10 +20,8 @@ const dataSource = new DataSource({
   logging: true,
   synchronize: false,
   schema: "transactions",
-  entities: ["src/database/entities/**/*entity.js"],
-  //entities: [UserEntity, TransactionEntity],
-  migrations: ["src/database/migrations/**/*.js"],
-  //migrations: [CreateTableUsers1689892033847, TransactionEntity1689895189070],
+  entities,
+  migrations,
   migrationsTableName: "my-migrations",
 });
 
